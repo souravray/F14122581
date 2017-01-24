81d8628893bcbf47106ba78c056a0348ed98aec2
Sourav Ray <me@raysourav.com>
2017-01-24 16:43:10 +0530

    First commit

---
+++ src/airprompt.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+//var cliMd = require("mdy");
+
+// var list = require('./lib/list');
+// var reader = require('./lib/details');
+// var reader = require('./lib/message');
